d0eea62d7e0313b11dd66d3af0b7fa20e7e3bceb
jolasman <jolasman@hotmail.com>
2014-04-17 23:13:45 +0100

    main
    estava a dar conflito o main

--- PROG_HANGMAN/main.cpp
+++ PROG_HANGMAN/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <conio.h>
 
 using namespace std;
@@ -9,6 +10,8 @@ using namespace std;
 #include "p4.h"
 #include "p5.h"
 #include "p6.h"
+#include "p7.h"
+#include "p8.h"
 
 void main () 
 {
@@ -23,5 +26,49 @@ void main ()
 	//p5_function();
 	//p6a_function();
 	//p6b_function();
+/*****************/ HEAD
+/**=======*/
+
+	/*Testes da p7a*/
+	/*int n;
+	string s;
+	vector<char> letters;
+
+	n = rand_int(10, 20);
+	cout << "Random number: " << n << endl << endl;
+
+	n = string_to_int("123");
+	cout << "Text converted to number: " << n << endl << endl;
+
+	s = int_to_string(10);
+	cout << "Number converted to text: " << s << endl << endl;
+
+	s = get_sentence("beatles.txt");
+	cout << "Random sentence: " << s << endl << endl;
+
+	s = string_to_upper("Abc xyZ");
+	cout << "Text converted to uppercase: " << s << endl << endl;
+
+	letters.push_back('F');
+	letters.push_back('T');
+	letters.push_back('P');
+	letters.push_back('L');
+	letters.push_back('O');
+	letters.push_back('E');
+	s = unhide_letters("Futebol Clube do Porto", letters);
+	cout << "Sentence: " << s << endl << endl;
+
+	show_vector_char(letters);
+	cout << endl;
+
+	bool exists = show_file("beatles.txt");
+	if (exists) cout << endl << "File exists!" << endl << endl; 
+	else cout << endl << "File doesn't exist." << endl << endl;*/
+
+	/*Testes da p7b*/
+	hangman();
+
 	_getch();  
-}
+}  
+
+/*este ficheiro estava a dar conflitos*/
